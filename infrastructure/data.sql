094c7ce317b536413fe50fea1edb66e6ab7ef878
MalenaMoix <malena.moix@overproofdev.com>
2021-09-28 20:36:23 -0300

    Add user_type and construction_type inserts on data.sql

--- infrastructure/data.sql
+++ infrastructure/data.sql
@@ -2,3 +2,8 @@ CREATE SCHEMA jms_dan;
 CREATE SCHEMA keycloak;
 
 GRANT ALL PRIVILEGES ON `jms_dan`.* TO 'root' @'%' WITH GRANT OPTION;
+
+CREATE TABLE IF NOT EXISTS jms_dan.user_type (id INT PRIMARY KEY, type TEXT);
+INSERT INTO jms_dan.user_type (id, type) VALUES (1, 'Cliente'), (2, 'Empleado');
+CREATE TABLE IF NOT EXISTS jms_dan.construction_type (id INT PRIMARY KEY, description TEXT);
+INSERT INTO jms_dan.construction_type (id, description) VALUES (1, 'Reforma'), (2, 'Casa'), (3, 'Edificio'), (4, 'Vial');
